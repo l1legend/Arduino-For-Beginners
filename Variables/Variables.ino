15dc46e55a8e92937a70e888c76a2323614e89f0
minTwin <michaelmin2020@gmail.com>
2022-12-05 04:57:38 -0600

    data types in C++

--- Variables/Variables.ino
+++ Variables/Variables.ino
@@ -1,14 +1,28 @@
 #define LED_PIN 12
 
-int a = 5;
+int a = 5; //-32,768 to 32,767
 int b;
+
+long l = 400000; // -2,147,483,648 to 2,147,483,647
+
+double c = 3.14;
+
+String d = "Hello Arduino";
+
+bool e = false;
+
+int userAge = 43;
+double temperature;
+String userName;
+bool isAlive;
 
 void setup() {
   // put your setup code here, to run once:
   a = 7 + 4;
   b = a + 4;
   Serial.begin(9600);
-  Serial.println(b);
+  //Serial.println(b);
+  Serial.println(l);
 }
 
 void loop() {
