00a8207648cb6fdb052abf1cc1a82918e543f24c
minTwin <michaelmin2020@gmail.com>
2022-12-06 19:00:00 -0600

    added some addition changes to the fade in and out code

--- LED Fade In and Out/LED Fade In and Out.ino
+++ LED Fade In and Out/LED Fade In and Out.ino
@@ -1,17 +1,18 @@
 #define LED_PIN 11
 
 void setup() {
-  // put your setup code here, to run once:
+  // Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   for (int i = 0; i <= 255; i++){
+    //Serial.println(i);
       analogWrite(LED_PIN, i); 
       delay(5);
   }
-  for ( int i = 255; i > 0; i--) {
+  for ( int i = 255; i >= 0; i--) {
+    //Serial.println(i);
       analogWrite(LED_PIN, i); 
       delay(5);
   }
