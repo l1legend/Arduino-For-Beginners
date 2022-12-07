d9c1a98e7e8032387b58f7182c7c7be014c80032
minTwin <michaelmin2020@gmail.com>
2022-12-07 08:58:46 -0600

    remove println statements

--- push button/push button.ino
+++ push button/push button.ino
@@ -2,18 +2,15 @@
 #define LED_PIN 11
 
 void setup() {
-  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
   pinMode (LED_PIN, OUTPUT);
 }
 
 void loop() {
   if (digitalRead(BUTTON_PIN) == HIGH) {
-    Serial.println("Button is pressed!");
     analogWrite(LED_PIN, 255);
   }
   else {
-    Serial.println("Button is not pressed.");
     analogWrite(LED_PIN, 0);
   }
   delay(100);
